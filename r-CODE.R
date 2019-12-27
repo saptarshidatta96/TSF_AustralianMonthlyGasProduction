6ea121cfd36f60678fb93ea4624ae44d8d7cfc68
saptarshidatta96 <datta.saptarshi96@gmail.com>
2019-12-27 13:45:17 +0530

    commit

--- r-CODE.R
+++ r-CODE.R
@@ -7,3 +7,9 @@ plot.ts(gas1)
 library(TTR)
 Gas.SMA <- SMA(gas1,n=13)
 plot.ts(Gas.SMA)
+
+# double exponential - models level and trend
+fit <- HoltWinters(gas1, gamma=FALSE)
+
+forecast(fit,3)
+plot(forecast(fit,3))
