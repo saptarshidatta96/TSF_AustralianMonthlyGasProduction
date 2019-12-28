fbbb49d1e48ab12caf8ea1654652f5691c5fd202
Saptarshi Datta <58257478+saptarshidatta96@users.noreply.github.com>
2019-12-28 13:22:37 +0530

    Add files via upload

---
+++ r-CODE.R
@@ -0,0 +1,45 @@
+install.packages("forecast")
+library(forecast)
+plot(gas)
+str(gas)
+gas1 <- ts(gas, start=c(1956),frequency =1)
+plot.ts(gas1)
+
+
+#create train and test data
+gas.train <- window(gas1, end = c(1993))
+gas.test<- window(gas1, start = c(1994))
+
+#simple moving average
+library(TTR)
+Gas.SMA <- SMA(gas1,n=13)
+ts.plot(Gas.SMA, gas1, gpars = list(col = c("red", "black")))
+
+#simple exponential smoothing #no trend no seasonality
+#constant forecast
+ses.gas = ses(gas.train, alpha = 0.2, h=3)
+autoplot(ses.gas)
+accuracy(ses.gas, gas.test)
+ses.gas$model
+ses.gas$fitted
+
+#lag 1 difference #ses on diff sales data
+gas.dif.train <- diff(gas.train)
+gas.dif.test <- diff(gas.test)
+
+autoplot(gas.dif.train)
+ses.gas.dif <- ses(gas.dif.train, h = 3)
+ses.gas.dif$model
+ses.gas.dif$fitted
+accuracy(ses.gas.dif, gas.dif.test)
+autoplot(ses.gas.dif)
+
+#holt's model with level and trend but no seasonality
+#double exponential smoothing
+holt.gas <- holt(gas.train, h = 3)
+autoplot(holt.gas)
+autoplot(forecast(holt.gas))
+holt.gas$model
+holt.gas$mean
+holt.gas$fitted
+accuracy(holt.gas, gas.test)
